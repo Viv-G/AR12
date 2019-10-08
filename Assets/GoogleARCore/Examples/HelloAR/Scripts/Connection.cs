ebd05cca81851e5956f295e4c39e7012a0eaddf1
Vivian Gauntlett <vivian.gauntlett@gmail.com>
2019-10-08 23:25:16 +1100

    Initial commit

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/Connection.cs
@@ -0,0 +1,52 @@
+﻿namespace GoogleARCore.Examples.HelloAR
+{
+/*    using System.Collections;
+    using System.Collections.Generic;
+    using System.Net; */
+    using System.Net.Sockets;
+    using System.Text;
+    using UnityEngine;
+
+    /// <summary>
+    /// Creates Connection to Server and sends data as string
+    /// </summary>
+    ///
+    public class Connection : MonoBehaviour
+    {
+        private static int port = 11111;
+        private static string HostIP = EntryScript.HostSet;
+        public static int pCount;
+        public static Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        public static void Connect(string Host)
+        {
+            Debug.Log("Establishing Connection to " + Host);
+            s.Connect(Host, port);
+            Debug.Log("Connection established \n");
+            if (s == null)
+                Debug.LogError("Connection failed");
+        }
+
+        public static void WriteString(int NPoints, string pointBuffer)
+        {
+        /*    if (pCount == 0)
+            {
+                s.Connect(HostIP, port);
+                pCount++;
+            } */
+            // Convert to Strings
+            string numPoints = NPoints.ToString() + " ENDN\n";
+            string buffSend = pointBuffer + " ENDP\n";
+            // Convert to Bytes
+            byte[] nSend = Encoding.ASCII.GetBytes(numPoints);
+            byte[] sBytes = Encoding.ASCII.GetBytes(buffSend);
+            int size = sBytes.Length;
+            string sizeSend = size.ToString();
+//            Debug.LogError(sizeSend);
+            //SEND
+            s.Send(nSend);
+            s.Send(sBytes);
+        }
+    }
+}
---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/EntryScript.cs
@@ -0,0 +1,152 @@
+﻿namespace GoogleARCore.Examples
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.SceneManagement;
+
+    public class EntryScript : MonoBehaviour
+    {
+        public static string HostSet = "172.20.10.2";
+        public Text Text_Status;
+        private string content;
+
+        public void SetIP(string HostField)
+        {
+            Debug.Log("Set IP \n");
+            HostSet = HostField;
+         //   Connection.Connect(HostSet);
+        }
+
+        public void LoadScene()
+        {
+            HelloAR.Connection.Connect(HostSet);
+
+            if (HelloAR.Connection.s == null)
+            {
+                content = "Unable To Connect... Try again";
+                return;
+            }
+            else
+            {
+                content = "Connected To: " + HostSet;
+                SceneManager.LoadScene("HelloAR");
+            }
+        }
+
+        public void LoadHome()
+        {
+            Debug.Log("Set IP \n");
+            HostSet = "192.168.8.100";
+            HelloAR.Connection.Connect(HostSet);
+            if (HelloAR.Connection.s == null)
+            {
+                content = "Unable To Connect... Try again";
+                return;
+            }
+            else
+            {
+                content = "Connected To: " + HostSet;
+                SceneManager.LoadScene("HelloAR");
+            }
+        }
+
+        public void LoadHotSpot()
+        {
+            Debug.Log("Set IP \n");
+            HostSet = "172.20.10.2";
+            HelloAR.Connection.Connect(HostSet);
+            if (HelloAR.Connection.s == null)
+            {
+                content = "Unable To Connect... Try again";
+                return;
+            }
+            else
+            {
+                content = "Connected To: " + HostSet;
+                SceneManager.LoadScene("HelloAR");
+            }
+        }
+
+        public void SetConf(string c_val)
+        {
+            float cfloat_val = float.Parse(c_val);
+            Common.PointcloudVisualizer.setConf = cfloat_val;
+            content = "Set Confidence To: " + c_val;
+            
+        }
+
+
+        /// <summary>
+        /// True if the app is in the process of quitting due to an ARCore connection error,
+        /// otherwise false.
+        /// </summary>
+        private bool m_IsQuitting = false;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Text_Status.text = content;
+
+            _UpdateApplicationLifecycle();
+
+            // If the player has not touched the screen, we are done with this update.
+            Touch touch;
+            if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+            {
+                return;
+            }
+
+        }
+        private void _UpdateApplicationLifecycle()
+        {
+            // Exit the app when the 'back' button is pressed.
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+
+            if (m_IsQuitting)
+            {
+                return;
+            }
+        }
+
+
+        /// <summary>
+        /// Actually quit the application.
+        /// </summary>
+        private void _DoQuit()
+        {
+            Application.Quit();
+        }
+
+        private void _ShowAndroidToastMessage(string message)
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity =
+                unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            if (unityActivity != null)
+            {
+                AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+                unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+                {
+                    AndroidJavaObject toastObject =
+                        toastClass.CallStatic<AndroidJavaObject>(
+                            "makeText", unityActivity, message, 0);
+                    toastObject.Call("show");
+                }));
+            }
+        }
+
+    }
+}
